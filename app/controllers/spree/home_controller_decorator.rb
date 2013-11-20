7420981548c485128f7a420a173321f4051b83ef
John Dyer <lbrapid@gmail.com>
2013-11-20 10:57:59 -0500

    Prevent duplicate products from showing up in the main products slider

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,7 +2,7 @@ Spree::HomeController.class_eval do
 
   def index
     slider = Spree::Taxon.where(:name => 'Slider').first
-    @slider_products = slider.products.active if slider
+    @slider_products = slider.products.active.distinct if slider
 
     featured = Spree::Taxon.where(:name => 'Featured').first
     @featured_products = featured.products if featured
