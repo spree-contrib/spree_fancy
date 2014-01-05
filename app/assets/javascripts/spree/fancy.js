8b62f665b5b00d289c82e3dff8ae15a6dab2e112
Jesse Gall <jesse@goodfellowsites.com>
2014-01-05 07:49:42 -0800

    fixes easy drop down at checkout
    Fixes #86
    Fixes #80
    
    Conflicts:
    	vendor/assets/javascripts/jquery.easydropdown.js

--- app/assets/javascripts/spree/fancy.js
+++ app/assets/javascripts/spree/fancy.js
@@ -85,3 +85,101 @@ $(function(){
   }).listen();
 
 });
+
+(function() {
+  Spree.ready(function($) {
+    var fillStates, getCountryId, updateState;
+    if (($('#checkout_form_address')).is('*')) {
+      ($('#checkout_form_address')).validate();
+      getCountryId = function(region) {
+        return $('#' + region + 'country select').val();
+      };
+      updateState = function(region) {
+        var countryId;
+        countryId = getCountryId(region);
+        if (countryId != null) {
+          if (Spree.Checkout[countryId] == null) {
+            return $.get(Spree.routes.states_search, {
+              country_id: countryId
+            }, function(data) {
+              Spree.Checkout[countryId] = {
+                states: data.states,
+                states_required: data.states_required
+              };
+              return fillStates(Spree.Checkout[countryId], region);
+            });
+          } else {
+            return fillStates(Spree.Checkout[countryId], region);
+          }
+        }
+      };
+
+      fillStates = function(data, region) {
+        var selected, stateInput, statePara, stateSelect, stateSpanRequired, states, statesRequired, statesWithBlank;
+        statesRequired = data.states_required;
+        states = data.states;
+        statePara = $('#' + region + 'state');
+        stateSelect = statePara.find('select');
+        stateInput = statePara.find('input');
+        stateSpanRequired = statePara.find('state-required');
+        if (states.length > 0) {
+          selected = parseInt(stateSelect.val());
+	  stateSelect.easyDropDown('destroy');
+          stateSelect.html('');
+          statesWithBlank = [
+            {
+              name: '',
+              id: ''
+            }
+          ].concat(states);
+          $.each(statesWithBlank, function(idx, state) {
+            var opt;
+            opt = ($(document.createElement('option'))).attr('value', state.id).html(state.name);
+            if (selected === state.id) {
+              opt.prop('selected', true);
+            }
+            return stateSelect.append(opt);
+          });
+	  var newDrop = stateSelect.easyDropDown({ cutOff: 10 });
+	  newDrop.easyDropDown('enable');
+          stateInput.hide().prop('disabled', true);
+          statePara.show();
+          stateSpanRequired.show();
+          if (statesRequired) {
+            stateSelect.addClass('required');
+          }
+	  stateSelect.removeClass('hidden');
+          return stateInput.removeClass('required');
+        } else {
+	  stateSelect.easyDropDown('disable');
+	  stateInput.show();
+          if (statesRequired) {
+            stateSpanRequired.show();
+            stateInput.addClass('required');
+          } else {
+            stateInput.val('');
+            stateSpanRequired.hide();
+            stateInput.removeClass('required');
+          }
+          statePara.toggle(!!statesRequired);
+          stateInput.prop('disabled', !statesRequired);
+          stateInput.removeClass('hidden');
+	  statePara.find('.dropdown').addClass('hidden');
+          return stateSelect.removeClass('required');
+        }
+      };
+      
+      ($('#bcountry select')).unbind("change");
+      ($('#scountry select')).unbind("change");
+      
+      ($('#bcountry select')).change(function() {
+        return updateState('b');
+      });
+      ($('#scountry select')).change(function() {
+        return updateState('s');
+      });
+      updateState('b');
+    }
+  });
+
+}).call(this);
