b4dff7fd04db2e39e4b4218c094f37110f3605f2
John Dyer <lbrapid@gmail.com>
2014-06-03 09:34:36 -0400

    Fix sliders when using postgres

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,13 +2,13 @@ Spree::HomeController.class_eval do
 
   def index
     slider = Spree::Taxon.where(:name => 'Slider').first
-    @slider_products = slider.products.active.uniq if slider
+    @slider_products = slider.products.active.to_a.uniq if slider
 
     featured = Spree::Taxon.where(:name => 'Featured').first
-    @featured_products = featured.products.active.uniq if featured
+    @featured_products = featured.products.active.to_a.uniq if featured
 
     latest = Spree::Taxon.where(:name => 'Latest').first
-    @latest_products = latest.products.active.uniq if latest
+    @latest_products = latest.products.active.to_a.uniq if latest
   end
 
 end
