3ce05304548722e01e6b935c45975d85ecd04ceb
Peter Berkenbosch <peter@pero-ict.nl>
2014-06-03 11:58:13 +0200

    get uniq products from taxons

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,13 +2,13 @@ Spree::HomeController.class_eval do
 
   def index
     slider = Spree::Taxon.where(:name => 'Slider').first
-    @slider_products = slider.products.active if slider
+    @slider_products = slider.products.active.uniq if slider
 
     featured = Spree::Taxon.where(:name => 'Featured').first
-    @featured_products = featured.products.active if featured
+    @featured_products = featured.products.active.uniq if featured
 
     latest = Spree::Taxon.where(:name => 'Latest').first
-    @latest_products = latest.products.active if latest
+    @latest_products = latest.products.active.uniq if latest
   end
 
 end
